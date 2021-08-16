43b57dc84f8e1213a2bca2fc5e9df85bf8256c36
e-dorigatti <emilio.dorigatti@gmail.com>
2021-08-16 10:34:25 +0200

    bugfix in snakefile

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='hyperband-snakemake',
-    version='0.0.4',
+    version='0.0.5',
     author='e-dorigatti',
     author_email='emilio.dorigatti@gmail.com',
     description='Orchestrate hyper-parameters search with snakemake',
